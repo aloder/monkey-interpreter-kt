9d6a98a3eaf10fff3fbcc0b0785781ec70f16a3e
Andrew Loder <aloder404@gmail.com>
2023-11-22 00:10:07 -0800

    first commit

---
+++ app/src/main/kotlin/interpreter/Token.kt
@@ -0,0 +1,22 @@
+package interpreter
+
+enum class TokenType(val literal: String) {
+  ILLEGAL("ILLEGAL"),
+  EOF("EOF"),
+  // Identifiers + literals
+  IDENT("IDENT"), // add, foobar, x, y, ...
+  INT("INT"), // 1343456
+  // Operators
+  ASSIGN("="),
+  PLUS("+"),
+  // Delimiters
+  COMMA(","),
+  SEMICOLON(";"),
+  LPAREN("("),
+  RPAREN(")"),
+  LBRACE("{"),
+  RBRACE("}"),
+  // Keywords
+  FUNCTION("FUNCTION"),
+  LET("LET")
+}
